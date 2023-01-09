676e8e5de6949d4657ebb687eb185d9ca7f882be
Joe Czepil <joeczepil@Joes-MacBook-Pro.local>
2023-01-09 07:36:03 -0800

    Pagination and school serializer

--- example/serializers/schools.py
+++ example/serializers/schools.py
@@ -3,13 +3,16 @@ from rest_framework import serializers
 from example.models import School, District
 
 
-class SchoolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = School
-        fields = ['name', 'category', 'district']
-
-
 class DistrictSerializer(serializers.ModelSerializer):
     class Meta:
         model = District
         fields = ['id', 'name', 'code']
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    district = DistrictSerializer(read_only=True)
+    district_id = serializers.IntegerField()
+
+    class Meta:
+        model = School
+        fields = ['name', 'category', 'district', 'district_id']
